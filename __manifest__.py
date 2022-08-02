4c8efc793484f60cca42d37aa120da227458904f
JuanEmus <emus75@hotmail.com>
2022-08-02 13:37:39 -0600

    [ADD] Subiendo modulo de productostransito

---
+++ __manifest__.py
@@ -0,0 +1,19 @@
+{
+    'name': 'Productos en tránsito',
+    'version': '13.0.0.2.5',
+    'author': 'JuanEmus',
+    'depends': [
+        'purchase',
+        'purchase_stock',
+    ],
+    'data': [
+        # security
+        # data
+        # demo
+        # reports
+        # views
+        'views/menu.xml',
+        'views/productostransito.xml',
+        'views/productostransitotablero.xml',
+    ],
+}
